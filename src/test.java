39a99fd439e94d0770b7eaf536fba933970d9ee3
Hagenbeck <peerschlieker@gmail.com>
2022-09-20 08:37:36 +0200

    small improvements

--- src/test.java
+++ src/test.java
@@ -1,12 +1,16 @@
 public class test {
     public static void main(String[] args){
         Vertex v1 = new Vertex(3, 4);
+        Vertex v2 = new Vertex(42, 0);
+        Vertex vv = new Vertex(1920,1080);
 
-        String sv = v1.toString();
-        System.out.println(sv);
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(vv);
 
-        double dis1 = v1.distToOrig();
-        System.out.println("Distance to Origin:" + dis1);
 
+        System.out.println("Distance to Origin(v1):" + v1.distToOrig());
+        System.out.println("Distance to Origin(v2):" + v2.distToOrig());
+        System.out.println("Distance to Origin(vv):" + vv.distToOrig());
     }
 }
