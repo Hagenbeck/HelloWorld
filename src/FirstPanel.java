3727cf9efcfaeaed26007f9aab92d2d59a9cb128
Hagenbeck <peerschlieker@gmail.com>
2022-10-06 14:31:14 +0200

    test

---
+++ src/FirstPanel.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class FirstPanel extends JPanel {
+    @Override
+    public void paintComponent(java.awt.Graphics g) {
+        g.fillRect(30,50,45,80);
+    }
+}
--- src/test.java
+++ src/test.java
@@ -1,9 +1,9 @@
+import javax.swing.*;
+
 public class test {
     public static void main(String[] args){
-        Vertex vTri = new Vertex(0,0);
-        double width = 100;
-        SimpleTriangle triAgain = new SimpleTriangle(vTri, width);
-        System.out.println(triAgain);
-        System.out.println("Area of triAgain: " + triAgain.area());
+        JFrame f = new JFrame("Hallo Welt");
+        f.add(new FirstPanel());
+        f.setVisible(true);
     }
 }
