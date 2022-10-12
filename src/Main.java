2d6495b54f958dfccdacb0b21b91729ed5ba15e6
Hagenbeck <peerschlieker@gmail.com>
2022-10-12 08:18:28 +0200

    new tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        ShowInFrame.show(new PaintablePanel(new PaintableOval(10,10,100,200)), "Titel");
+        PaintablePanel p = new PaintablePanel(new PaintableRectangle(10,10,100,200));
+        ShowInFrame.show(p, "Titel");
     }   
 }
