264870be0a7c0afa3ba5f9b2fe336c7463e22ec8
Hagenbeck <peerschlieker@gmail.com>
2022-10-16 19:10:02 +0200

    got the Paintable Star to work

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.awt.Color;
 public class Main {
     public static void main(String[] args){
-        String url = "wasserfall.jpg";
+        /*String url = "wasserfall.jpg";
         GeometricImage gi = new GeometricImage(url,0, 0);
         PaintablePanel p = new PaintablePanel(gi);
-        
+        */
+
+        PaintablePanel p = new PaintablePanel(new PaintableStar(200, 100, 100, Color.green));
+
         ShowInFrame.show(p, "Bild");      
         
-        
-        System.out.println(gi.getHeigth());
     }   
 }
